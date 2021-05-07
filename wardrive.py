018806827643be83c88ba37dfcde485c87621bee
tjbishop@gmail.com <tjbishop@gmail.com>
2021-05-07 12:11:59 -0700

    updated log test

--- wardrive.py
+++ wardrive.py
@@ -53,5 +53,5 @@ class Wardrive(plugins.Plugin):
         now = datetime.datetime.now()
         time_rn = now.strftime(self.date_format + "\n%I:%M %p")
         pos = self.coordinates
-        logging.info("Pwnagotchi [Wardrive] POS " + pos)
+        logging.info("[Wardrive]] (pos: %d)" % (pos))
         ui.set('clock', time_rn)
